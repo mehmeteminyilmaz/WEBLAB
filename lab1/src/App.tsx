1eab7ec3c5f136f8e907cf16906eef2b1ee52497
Mehmet Emin Yılmaz <m.eminylmz2303@gmail.com>
2026-02-17 15:52:28 +0300

    main
    main

---
+++ lab1/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+return (
+<div>
+<h1>Web Tasarimi ve Programlama </h1>
+<h2>LAB -1</h2>
+<p>Ad Soyad: [Mehmet Emin Yilmaz]</p>
+<p>Ogrenci No: [235541078] </p>
+</div>
+ )
+ }
+
+export default App
